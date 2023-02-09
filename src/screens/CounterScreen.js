83104e7f6ae38c369d21e6ed505c72ada12a9ba9
gabriellgrehov <eugenekalnoy@gmail.com>
2023-02-09 17:15:31 +0200

    ту

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,18 +1,32 @@
-import React, {useState} from "react";
+import React, {useReducer} from "react";
 import { View, Text, StyleSheet, Button} from "react-native";
+import { step0 } from "react-native/Libraries/Animated/Easing";
+
+const reducer = (state, action) => {
+
+    switch(action.type){
+        case 'increment':
+            return {...state, count: state.count + action.payload}
+        case 'decrement':
+            return {...state, count: state.count - action.payload}
+        default: 
+            state;
+    }
+
+}
 
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
-    
+    const [state, dispatch] = useReducer(reducer, { count: 0 })
+
     return (
             <View>
                 <Button title="Increase" onPress={() => {
-                    setCounter(counter + 1);
+                    dispatch({type: 'increament', payload: 1})    
                 }}/>
                 <Button title="Decrease" onPress={() => {
-                    setCounter(counter - 1);
+                    
                 }}/>
-                <Text>Current count: {counter}</Text>
+                <Text>Current count: {}</Text>
             </View>
         );
 };
